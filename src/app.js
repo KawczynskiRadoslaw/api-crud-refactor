484d57d668fd968cb53573f8d7c7da3e06f18e78
Mateusz Bogolubow <mateusz@bogolubow.com>
2020-08-11 14:21:52 +0200

    Init

---
+++ src/app.js
@@ -0,0 +1,16 @@
+// ./src/app.js
+import API from './API';
+import Excursions from './Excursions';
+
+document.addEventListener('DOMContentLoaded', init);
+
+function init() {
+    console.log('DOM');
+
+    const api = new API();
+    const excursions = new Excursions(api);
+    excursions.load();
+    excursions.remove();
+    excursions.add();
+    excursions.update();
+}
